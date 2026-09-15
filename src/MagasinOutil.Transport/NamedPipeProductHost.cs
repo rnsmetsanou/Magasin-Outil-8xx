3908b42cfd92268d0f77a82a51adb606627ba9be
rnsmetsanou <ernest.metsanou@willemin-macodel.com>
2026-09-15 12:34:18 +0200

    demo8xx: host session-governed product magazine reads

--- src/MagasinOutil.Transport/NamedPipeProductHost.cs
+++ src/MagasinOutil.Transport/NamedPipeProductHost.cs
@@ -12,11 +12,16 @@ namespace MagasinOutil.Transport;
 public static class NamedPipeProductHost
 {
     [SupportedOSPlatform("windows")]
-    public static WebApplication Create(string pipeName, IToolInventoryReader inventory, IProductSessionService sessions)
+    public static WebApplication Create(
+        string pipeName,
+        IToolInventoryReader inventory,
+        IProductSessionService sessions,
+        IProductMagazineReadService magazineReads)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(pipeName);
         ArgumentNullException.ThrowIfNull(inventory);
         ArgumentNullException.ThrowIfNull(sessions);
+        ArgumentNullException.ThrowIfNull(magazineReads);
 
         var builder = WebApplication.CreateSlimBuilder(new WebApplicationOptions { Args = [] });
         builder.Configuration.Sources.Clear();
@@ -25,6 +30,7 @@ public static class NamedPipeProductHost
             options.ListenNamedPipe(pipeName, endpoint => endpoint.Protocols = HttpProtocols.Http2));
         builder.Services.AddSingleton(inventory);
         builder.Services.AddSingleton(sessions);
+        builder.Services.AddSingleton(magazineReads);
         builder.Services.AddGrpc(options =>
         {
             options.MaxReceiveMessageSize = 16 * 1024;
@@ -33,8 +39,11 @@ public static class NamedPipeProductHost
         });
 
         var app = builder.Build();
+        // Legacy pilot read remains available for regression proof. The integrated HMI uses the
+        // session-governed ProductMagazineGrpcService below.
         app.MapGrpcService<InventoryGrpcService>();
         app.MapGrpcService<ProductSessionGrpcService>();
+        app.MapGrpcService<ProductMagazineGrpcService>();
         return app;
     }
 }
